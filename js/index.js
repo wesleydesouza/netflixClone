a91eec16555c755fea841c872c2e4b359a5bfa17
WesleydeSouza <wesley_duarte122@hotmail.com>
2021-10-21 10:48:36 -0300

    images correction

--- js/index.js
+++ js/index.js
@@ -2,46 +2,46 @@ const imagens = [
     lista1 = [
         imagem1 = {
             nome: "Anjos da Noite",
-            imagem: "../img/card_1.png"
+            imagem: "img/card_1.png"
         },
         imagem2 = {
             nome: "VanHelsing",
-            imagem: "../img/card_2.png"
+            imagem: "img/card_2.png"
         },
         imagem3 = {
             nome: "Padre",
-            imagem: "../img/card_3.png"
+            imagem: "img/card_3.png"
         },
         imagem4 = {
             nome: "IT",
-            imagem: "../img/card_4.png"
+            imagem: "img/card_4.png"
         },
         imagem5 = {
             nome: "Guerra Mundial Z",
-            imagem: "../img/card_5.png"
+            imagem: "img/card_5.png"
         }
     ],
 
     lista2 = [
         imagem1 = {
             nome: "Anjos da Noite",
-            imagem: "../img/card_6.png"
+            imagem: "img/card_6.png"
         },
         imagem2 = {
             nome: "VanHelsing",
-            imagem: "../img/card_7.png"
+            imagem: "img/card_7.png"
         },
         imagem3 = {
             nome: "Padre",
-            imagem: "../img/card_8.png"
+            imagem: "img/card_8.png"
         },
         imagem4 = {
             nome: "IT",
-            imagem: "../img/card_9.png"
+            imagem: "img/card_9.png"
         },
         imagem5 = {
             nome: "Guerra Mundial Z",
-            imagem: "../img/card_10.jpg"
+            imagem: "img/card_10.jpg"
         }
     ]
 
